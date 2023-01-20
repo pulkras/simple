67ddc2d7938745da9c96cdf8ad907312619a59c0
pulkras <maligin128@gmail.com>
2023-01-20 06:00:36 +0000

    simple

---
+++ Simple.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Simple {
+    public static void main(String[] args) {
+        Scanneer scanner = new Scanner(System.in);
+
+        System.out.println("Write three-sign number\n")
+
+        int yourNumber = scanner.neextInt();
+
+        System.out.println(yourNumber);
+    }
+}
+
